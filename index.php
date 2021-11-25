b0f8f6b4efe58807e9f096ff56150357b726f22f
ViniciusWitzgall <viniwitz@gmail.com>
2021-11-25 16:10:08 -0300

    adicionado a classe Ususario

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 require_once("config.php");
 
+$vinicius = new Usuario();
+
+$vinicius->loadbyId(3);
+
+echo $vinicius;
+
+
+/*
+EXEMPLO DO sQL
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-
+*/
 ?>
